676f83bf74a232258797d2474dfd8e3bb16de27a
taha <taha@dmdmax.com>
2022-05-16 00:03:00 +0500

    blacklist option added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 function App() {
   const [report, setReport] = useState('');
   const [listFile, setListFile] = useState([]);
+  const [emails, setEmails] = useState('');
   
   const onDrop = useCallback(acceptedFiles => {
     // Do something with the files
@@ -17,17 +18,19 @@ function App() {
 
   const handleSubmit = () => {
     let baseUrl = 'http://3.122.255.224:3011';
-
+    let emailsArray = emails.split(',');
     
     // if(report === 'purge') baseUrl = 'http://localhost:3011';
     // else if(report === 'revReport') baseUrl = 'http://localhost:3011';
 
     let formData = new FormData();
     formData.append("file", listFile);
+    if(report === 'revReport') formData.append("emails", emailsArray);
 
     Axios.post(`${baseUrl}/history/automatedReport?report=${report}`, formData)
     .then(res => {
       console.log("response", res);
+      alert(report === 'revReport' ? 'Please check your inbox.' : res.data.message);
     })
     .catch(err => {
       console.log(err);
@@ -51,7 +54,21 @@ function App() {
             <input type="radio" name="report" value={report} checked={report === 'revReport' ? true : false} onChange={()=> setReport('revReport')} />
             <label>Revenue Report</label>
           </div>
+
+          <div style={{marginLeft: "2vw", display: "inline-block"}}>
+            <input type="radio" name="report" value={report} checked={report === 'blacklist' ? true : false} onChange={()=> setReport('blacklist')} />
+            <label>Blacklist</label>
+          </div>
         </div>
+
+        {/* {report === 'revReport' ?
+          <div style={{marginTop: "4vh"}}>
+            <label>Emails to send the report to (multiple emails should be comma separated):</label>
+            <input name="emails" value={emails} onChange={(e)=> setEmails(e.target.value)} />
+          </div>
+          :
+          ''
+        } */}
         
         <div style={{marginTop: "4vh"}}>
           <button style={{cursor: "pointer", backgroundColor: "purple", color: "magenta", padding: "2vh 2vw", borderRadius: "8px", border: 0}} onClick={handleSubmit}>Submit</button>
